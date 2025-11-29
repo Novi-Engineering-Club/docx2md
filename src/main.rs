ea0b2e0254b2a82d1a272c4c845657fc07ececc7
smit4k <smit@smit.codes>
2025-11-29 14:08:12 -0500

    Get user input for output directory name

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,31 @@ use pandoc::PandocOption;
 use std::path::PathBuf;
 use std::fs;
 use std::env;
+use std::io;
 
 fn main() {
-    fs::create_dir_all("output").unwrap();
-    env::set_current_dir("output").unwrap();
+    let mut output_dir_name = String::new();
+    io::stdin().read_line(&mut output_dir_name).expect("Failed to read line!");
+    
+    let output_dir_name = output_dir_name.trim();
+
+    let docx_path = format!("{}.docx", output_dir_name);
+
+    if !PathBuf::from(&docx_path).exists() {
+        eprintln!("Error: {} does not exist", docx_path);
+        return;
+    }
+    
+    fs::create_dir_all(&output_dir_name).unwrap();
+    env::set_current_dir(&output_dir_name).unwrap();
+    
     let mut pandoc = pandoc::new();
-    pandoc.add_input("../example.docx");
+    
+    pandoc.add_input(&format!("../{}.docx", output_dir_name));
     pandoc.add_option(PandocOption::ExtractMedia(PathBuf::from(".")));
+    
     pandoc.set_output_format(OutputFormat::MarkdownGithub, vec![]);
-    pandoc.set_output(OutputKind::File(PathBuf::from("example.md")));
+    
+    pandoc.set_output(OutputKind::File(PathBuf::from(format!("{}.md", output_dir_name))));
     pandoc.execute().unwrap();
 }
