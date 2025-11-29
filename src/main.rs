6bbc2a481b8efe3d9a5413385c4080f29bb4dd82
smit4k <smit@smit.codes>
2025-11-29 14:16:49 -0500

    Update formatting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use pandoc::OutputKind;
 use pandoc::OutputFormat;
+use pandoc::OutputKind;
 use pandoc::PandocOption;
-use std::path::PathBuf;
-use std::fs;
 use std::env;
+use std::fs;
 use std::io;
+use std::path::PathBuf;
 
 fn main() {
     let mut output_dir_name = String::new();
-    io::stdin().read_line(&mut output_dir_name).expect("Failed to read line!");
-    
+    io::stdin()
+        .read_line(&mut output_dir_name)
+        .expect("Failed to read line!");
+
     let output_dir_name = output_dir_name.trim();
 
     let docx_path = format!("{}.docx", output_dir_name);
@@ -18,17 +20,20 @@ fn main() {
         eprintln!("Error: {} does not exist", docx_path);
         return;
     }
-    
+
     fs::create_dir_all(&output_dir_name).unwrap();
     env::set_current_dir(&output_dir_name).unwrap();
-    
+
     let mut pandoc = pandoc::new();
-    
+
     pandoc.add_input(&format!("../{}.docx", output_dir_name));
     pandoc.add_option(PandocOption::ExtractMedia(PathBuf::from(".")));
-    
+
     pandoc.set_output_format(OutputFormat::MarkdownGithub, vec![]);
-    
-    pandoc.set_output(OutputKind::File(PathBuf::from(format!("{}.md", output_dir_name))));
+
+    pandoc.set_output(OutputKind::File(PathBuf::from(format!(
+        "{}.md",
+        output_dir_name
+    ))));
     pandoc.execute().unwrap();
 }
